1138f2e53a77e6dff0554822c89ccb895faf6387
BrunoBSchuller <brunschuller@gmail.com>
2025-03-20 20:07:23 -0300

    Imprementada V1 com entrada e saida de daos

--- JogoDeAdivinhacao.ConsoleApp/Program.cs
+++ JogoDeAdivinhacao.ConsoleApp/Program.cs
@@ -2,9 +2,26 @@
 {
     internal class Program
     {
+        // V1 : ESTRUTURA BASICA E ENTRADA DO USUARIO
         static void Main(string[] args)
+        
+        
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("----------------------------------------------");
+            Console.WriteLine("Jogo da Adivinhação");
+            Console.WriteLine("----------------------------------------------");
+
+
+            //Lógica do jogo
+            Console.Write("Digite um número para chutar: ");
+            int numeroDigitado = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine("----------------------------------------------");
+            Console.WriteLine("Você digitou o número: " + numeroDigitado); 
+
+
+            Console.ReadLine();
         }
     }
 }
