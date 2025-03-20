df3e336b6b0cf5a8449e4f63ffabbd32a68d3540
BrunoBSchuller <brunschuller@gmail.com>
2025-03-20 20:34:54 -0300

    Implementar v3 com variação de entrada

--- JogoDeAdivinhacao.ConsoleApp/Program.cs
+++ JogoDeAdivinhacao.ConsoleApp/Program.cs
@@ -13,13 +13,35 @@
 
 
             //Lógica do jogo
-            Console.Write("Digite um número para chutar: ");
+            Random geradorDeNumeros = new Random();
+             
+            int numeroSecreto = geradorDeNumeros.Next(1, 21);
+
+            Console.Write("Digite um número (de 1 á 20) para chutar: ");
             int numeroDigitado = Convert.ToInt32(Console.ReadLine());
 
 
             Console.WriteLine("----------------------------------------------");
             Console.WriteLine("Você digitou o número: " + numeroDigitado); 
 
+            if (numeroDigitado == numeroSecreto)
+            {
+                Console.WriteLine("----------------------------------------------");
+                Console.WriteLine("Parabéns, você acertou!");
+                Console.WriteLine("----------------------------------------------");
+            }                
+            else if (numeroDigitado > numeroSecreto)
+            {
+                Console.WriteLine("----------------------------------------------");
+                Console.WriteLine("O número digitado foi maior que o número secreto!");
+                Console.WriteLine("----------------------------------------------");
+            }
+            else
+            {
+                Console.WriteLine("----------------------------------------------");
+                Console.WriteLine("O número digitado foi menor que o número secreto!");
+                Console.WriteLine("----------------------------------------------");
+            }
 
             Console.ReadLine();
         }
